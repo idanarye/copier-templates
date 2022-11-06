852485297fc65351d382bd91e5e685298a30ab9f
Idan Arye <idanarye@weka.io>
2022-11-06 12:19:11 +0200

    Update paths in the Java template

--- project-in-language/java/post_gen_project.sh
+++ project-in-language/java/post_gen_project.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-ORIG="`cat src/main/java/App.java`"
+ORIG="`cat app/src/main/java/App.java`"
 
 echo | gradle init --project-name . --type java-application --dsl kotlin --test-framework junit
 
 # sed -i -e 's/rootProject.*//' settings.gradle.kts
 sed -i -e '/rootProject/d' settings.gradle.kts
 
-echo "$ORIG" > src/main/java/App.java
-sed -i -e '/    /d' src/test/java/AppTest.java
+echo "$ORIG" > app/src/main/java/App.java
+sed -i -e '/    /d' app/src/test/java/AppTest.java
