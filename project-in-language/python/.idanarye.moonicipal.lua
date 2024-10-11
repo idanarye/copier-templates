e053df583e96c5506bcaf2df7700d6f0c7cd4c3a
Idan Arye <IdanArye@gmail.com>
2024-10-11 05:00:01 +0300

    Make the Python project template use the new Moonicipal lib import mechanism

--- project-in-language/python/.idanarye.moonicipal.lua
+++ project-in-language/python/.idanarye.moonicipal.lua
@@ -1,5 +1,6 @@
 local moonicipal = require'moonicipal'
+---@diagnostic disable-next-line: unused-local
 local T = moonicipal.tasks_file()
 
-moonicipal.include(require'idan.project.python.with_uv' {
-})
+---@diagnostic disable-next-line: unused-local
+local P, cfg = moonicipal.import(require'idan.project.python.with_uv')
