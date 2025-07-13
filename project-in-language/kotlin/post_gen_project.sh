1ab5983f0f957527fc1187366dc7d8a929adb76f
Idan Arye <IdanArye@gmail.com>
2025-07-13 17:12:21 +0300

    Update the Kotlin project template

--- project-in-language/kotlin/post_gen_project.sh
+++ project-in-language/kotlin/post_gen_project.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-gradle init --type kotlin-application --dsl groovy < /dev/null
-rm app/src/test/kotlin/AppTest.kt
-cat > app/src/main/kotlin/App.kt <<-EOF
-fun main(args: Array<String>) {
-}
-EOF
+gradle init --type kotlin-application --dsl groovy --use-defaults --overwrite # < /dev/null
+#rm app/src/test/kotlin/AppTest.kt
+# cat > app/src/main/kotlin/App.kt <<-EOF
+# fun main(args: Array<String>) {
+# }
+# EOF
