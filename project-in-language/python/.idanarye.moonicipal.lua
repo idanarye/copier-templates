f15aa055ffcadd65cd3d4f8a7a8843a4ba10855b
Idan Arye <idanarye@weka.io>
2026-02-03 19:21:54 +0200

    Add `link_easypy` task to Python Moonicipal file

--- project-in-language/python/.idanarye.moonicipal.lua
+++ project-in-language/python/.idanarye.moonicipal.lua
@@ -2,5 +2,11 @@ local moonicipal = require'moonicipal'
 ---@diagnostic disable-next-line: unused-local
 local T = moonicipal.tasks_file()
 
+local channelot = require'channelot'
+
 ---@diagnostic disable-next-line: unused-local
 local P, cfg = moonicipal.import(require'idan.project.python.with_uv')
+
+function T:link_easypy()
+    channelot.windowed_terminal_job{'ln', '--verbose', '--symbolic', '/files/code/wekapp/easypy/', 'src/'}
+end
